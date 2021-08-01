c5f5f965cd6f145ac61a335c0b2b7afbdd6264aa
Claudia <cmjengelsman@gmail.com>
2021-08-01 17:04:26 +1200

    At intro section, fix mobile padding on container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,24 @@
     <section class="homepage-header">
       <img class="header-image" alt="Runner in a field with mountains in the back" src="../assets/headerhomepagerunner.jpg" width="1600" />
     </section>
-    <section class="container">
+
+    <section class="vision-values">
+      <div class="container text-img-split">
+        <div class="welcome">
+          <h2 class="emphasize green">Welcome to Tower Junction Physio</h2>
+          <p>Here comes a super nice blurb to welcome website visitors.</p>
+          <p>
+            At Tower Junction Physio our aim is to help you recover from a (sports) injury in a timely and effective way, so you can move
+            freely and painfree again. We specialize in shoulder, neck and feet, but with our many years of experience we can assist in many
+            other areas as well. Feel free to call us and discuss how we can best help you.
+          </p>
+        </div>
+
+        <img src="../assets/placeholderphoto.png" alt="placeholder photo, nothing to see here unfortunately" class="photo" />
+      </div>
+    </section>
+    <!-- <section class="container">
       <h1>Hi there!</h1>
-      <p>
-        This page has not yet received much love and attention. The other pages, which you can access through the menu above, have more
-        information and features to look at.
-      </p>
       <p>Some features that might be a nice idea for this homepage:</p>
       <ul>
         <li>section that shares a little about the team or the practise with a link to the about page</li>
@@ -19,8 +31,8 @@
         <li>
           A small bar with a warning for covid-19 updates that refers to the new page, could be turned off and on as situation changes
         </li>
-      </ul>
-      <!-- <p>Other ideas that could be nice:</p>
+      </ul> -->
+    <!-- <p>Other ideas that could be nice:</p>
       <ul>
         <li>An icon that shows in the tab of the browser instead of the default one</li>
         <li>Link to Google maps for the location</li>
@@ -36,7 +48,7 @@
         If you're curious about the code, I have no secrets, it's properly structured and documented and it is available publicly on Github
         <a href="https://github.com/Claudia413/tjp-website/">here</a>
       </p> -->
-    </section>
+    <!-- </section> -->
     <section class="contact cta">
       <img class="cta-img" src="../assets/FieldHike.jpg" />
       <div class="overlay">
@@ -115,7 +127,7 @@ h2 {
   width: 100%;
   position: relative;
   .container {
-    padding: 40px 0;
+    padding-top: 40px;
   }
 }
 .cta-img {
@@ -130,7 +142,6 @@ h2 {
   height: 100%;
   z-index: -1;
 }
-
 .overlay {
   width: 100%;
   height: 100%;
@@ -142,5 +153,51 @@ h2 {
     rgba(255, 255, 255, 1) 40%,
     rgba(255, 255, 58, 0) 100%
   ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+}
+.text-img-split {
+  display: grid;
+  column-gap: 40px;
+  grid-template-columns: 60% 40%;
+  grid-template-rows: auto auto;
+  grid-template-areas: "vision img";
+  .welcome {
+    padding: 80px 0;
+    grid-area: vision;
+    margin-bottom: 80px;
+  }
+  .photo {
+    grid-area: img;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  h2 {
+    margin-top: 0;
+  }
+  @media screen and(max-width:1200px) {
+    grid-template-columns: 60% 40%;
+  }
+  @media screen and(max-width:991px) {
+    column-gap: 0;
+    grid-template-areas:
+      "vision vision"
+      "img img";
+    .photo {
+      grid-area: img;
+      display: block;
+      position: relative;
+      left: -80px;
+      width: calc(100% + 160px);
+      height: auto;
+      object-fit: cover;
+    }
+  }
+  @media screen and(max-width: 768px) {
+    .photo {
+      position: relative;
+      left: -32px;
+      width: calc(100% + 64px);
+    }
+  }
 }
 </style>
