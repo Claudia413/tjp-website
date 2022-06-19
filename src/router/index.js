8ce74357790fa30e178db428a2b296f6ba1a807f
Claudia <cmjengelsman@gmail.com>
2022-06-19 14:33:32 +1200

    Fix scrolling issue for mobile

--- src/router/index.js
+++ src/router/index.js
@@ -44,6 +44,13 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
 
 export default router
